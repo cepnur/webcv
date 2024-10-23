2f483e5d242b0fa99be9968628df556336fe67aa
cepnur <muhamadcepnur@gmail.com>
2024-10-23 12:30:51 +0700

    edit wa

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <Skill />
     <Footer />
     <!-- Icon WhatsApp melayang di sudut kanan bawah -->
-    <a :href="whatsappLink" target="_blank" class="fixed bottom-10 right-20 text-white rounded-full shadow-lg transition" title="Chat with me on WhatsApp">
+    <a :href="whatsappLink" target="_blank" class="fixed bottom-10 right-10 text-white rounded-full shadow-lg transition" title="Chat with me on WhatsApp">
       <!-- Gunakan Font Awesome untuk ikon WhatsApp -->
       <font-awesome-icon icon="fa-brands fa-whatsapp" class="text-3xl w-8 h-8 p-2 md:w-10 md:h-10 md:p-4 bg-green-500 rounded-full hover:<p>text me</p>" />
     </a>
